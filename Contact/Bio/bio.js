a5f3b6411ce7db0726874844e59c699834a6b40e
ohheyimjen <jendority@gmail.com>
2019-03-21 09:30:38 -0700

    Created bio.js. Accordions working. Needs styling.

--- Contact/Bio/bio.js
+++ Contact/Bio/bio.js
@@ -1,1 +1,18 @@
-//testing
+class Bio {
+    constructor(dropDown) {
+        this.dropDown = dropDown;
+        this.aboutBtn = this.dropDown.querySelector('.aboutBtn');
+        this.aboutBtn.innerText = 'About';
+        this.aboutBtn.addEventListener('click', () => {
+            this.expandBio() 
+        });
+    }
+
+   expandBio() {
+       this.dropDown.classList.toggle('bio-open');
+   } 
+}
+
+let bios = document.querySelectorAll('.bio').forEach(bio => {
+    new Bio(bio);
+});
